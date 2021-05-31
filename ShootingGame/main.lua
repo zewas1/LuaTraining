b3ec891da818d86c016dc65c2718536aa71089d0
zewas1 <matas.budriunas1@gmail.com>
2021-05-31 21:17:29 +0300

    sprites and functionalities

--- ShootingGame/main.lua
+++ ShootingGame/main.lua
@@ -5,27 +5,44 @@ function love.load()
     target.radius = 50
 
     score = 0
-    timer = 0
+    timer = 10
     gameFont = love.graphics.newFont(40)
+
+    sprites = {}
+    sprites.sky = love.graphics.newImage('sprites/sky.png')
+    sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png')
+    sprites.target = love.graphics.newImage('sprites/target.png')
+
+    love.mouse.setVisible(false)
 end
 
 function love.update(dt)
-
+    if timer > 0 then
+        timer = timer - dt
+    end
+    if timer < 0 then
+        timer = 0
+    end
 end
 
 function love.draw()
-    love.graphics.setColor(0.5,0,0)
-    love.graphics.circle("fill", target.x, target.y, target.radius)
+    love.graphics.draw(sprites.sky,0,0)
 
     love.graphics.setColor(1,1,1)
     love.graphics.setFont(gameFont)
     love.graphics.print(score, 0, 0)
+    love.graphics.print(math.ceil(timer), 300, 0)
+
+    love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+    love.graphics.draw(sprites.crosshairs, love.mouse.getX()-20, love.mouse.getY()-20)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
     local mouseToTarget = distanceBetween(x, y, target.x, target.y)
     if button == 1 and mouseToTarget < target.radius then
         score = score + 1
+        target.x = math.random(target.radius,love.graphics.getWidth()-target.radius) 
+        target.y = math.random(target.radius,love.graphics.getHeight()-target.radius)
     end
 end
 
